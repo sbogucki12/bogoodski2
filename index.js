20f2c3274497b66d384e6dc69bb6a12b502eb423
Steve Bogucki <sbogucki@mail.usf.edu>
2018-06-20 18:12:04 -0700

    tested contact page

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const app = express();
 
 const FormMessage = mongoose.model('formMessages')
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 
 app.use(bodyParser.urlencoded({
     extended: true
