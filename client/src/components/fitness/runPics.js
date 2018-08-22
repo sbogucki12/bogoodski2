db8976159e893b6b569f307a1eddc10e303db98f
Steve Bogucki <sbogucki@mail.usf.edu>
2018-08-21 17:54:12 -0700

    updated run log in fitness component

--- client/src/components/fitness/runPics.js
+++ client/src/components/fitness/runPics.js
@@ -1,10 +1,15 @@
+import Aug21Run from '../../content/images/fitness/runAug212018.JPG';
 import Aug20Run from '../../content/images/fitness/runAug202018.JPG';
 import Aug19Run from '../../content/images/fitness/runAug192018.JPG';
 import Aug18Run from '../../content/images/fitness/runAug182018.JPG';
 import Aug17Run from '../../content/images/fitness/runAug172018.JPG';
-import Aug16Run from '../../content/images/fitness/runAug162018.JPG';
 
 export const runPics = [
+    {
+        image: Aug21Run,
+        date: 'Aug 21, 2018',
+        name: 'Aug212018'
+    },
     {
         image: Aug20Run,
         date: 'Aug 20, 2018',
@@ -24,11 +29,6 @@ export const runPics = [
         image: Aug17Run,
         date: 'Aug 17, 2018',
         name: 'Aug172018'
-    },
-    {
-        image: Aug16Run,
-        date: 'Aug 16, 2018',
-        name: 'Aug162018'
     }
 ];
 
