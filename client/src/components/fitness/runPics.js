2807a5096afa159e7cbaf08d4e27b837d01cc50f
Steve Bogucki <sbogucki@mail.usf.edu>
2018-09-12 20:59:00 -0700

    updated run log

--- client/src/components/fitness/runPics.js
+++ client/src/components/fitness/runPics.js
@@ -1,10 +1,15 @@
+import Sep12Run from '../../content/images/fitness/runSep1220182.JPG';
 import Sep11Run from '../../content/images/fitness/runSep1120182.JPG';
 import Sep10Run from '../../content/images/fitness/runSep102018.JPG';
 import Sep09Run1 from '../../content/images/fitness/runSep0920181.JPG';
 import Sep09Run2 from '../../content/images/fitness/runSep0920182.JPG';
-import Sep08Run from '../../content/images/fitness/runSep082018.JPG';
 
 export const runPics = [
+    {
+        image: Sep12Run,
+        date: 'Sept. 12, 2018',
+        name: 'Sep122018'
+    },
     {
         image: Sep11Run,
         date: 'Sept. 11, 2018',
@@ -24,11 +29,6 @@ export const runPics = [
         image: Sep09Run1,
         date: 'Sept. 9, 2018 (1 of 2)',
         name: 'Sep092018Run1'
-    },
-    {
-        image: Sep08Run,
-        date: 'Sept. 8, 2018',
-        name: 'Sep082018'
-    }
+    }    
 ];
 
