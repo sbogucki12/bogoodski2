18f79fccd1f7c1cff9d8b59ad9e7727d2bb25a27
Steve Bogucki <sbogucki@mail.usf.edu>
2018-09-16 19:39:56 -0700

    updated run log

--- client/src/components/fitness/runPics.js
+++ client/src/components/fitness/runPics.js
@@ -1,34 +1,34 @@
+import Sep16Run from '../../content/images/fitness/runSep1620182.JPG';
+import Sep15Run from '../../content/images/fitness/runSep1520182.JPG';
+import Sep14Run from '../../content/images/fitness/runSep1420182.JPG';
+import Sep13Run from '../../content/images/fitness/runSep1320182.JPG';
 import Sep12Run from '../../content/images/fitness/runSep1220182.JPG';
-import Sep11Run from '../../content/images/fitness/runSep1120182.JPG';
-import Sep10Run from '../../content/images/fitness/runSep102018.JPG';
-import Sep09Run1 from '../../content/images/fitness/runSep0920181.JPG';
-import Sep09Run2 from '../../content/images/fitness/runSep0920182.JPG';
 
 export const runPics = [
+    {
+        image: Sep16Run,
+        date: 'Sept. 16, 2018',
+        name: 'Sep162018'
+    },
+    {
+        image: Sep15Run,
+        date: 'Sept. 15, 2018',
+        name: 'Sep152018'
+    },
+    {
+        image: Sep14Run,
+        date: 'Sept. 14, 2018',
+        name: 'Sep142018'
+    },
+    {
+        image: Sep13Run,
+        date: 'Sept. 13, 2018',
+        name: 'Sep132018'
+    },
     {
         image: Sep12Run,
         date: 'Sept. 12, 2018',
         name: 'Sep122018'
-    },
-    {
-        image: Sep11Run,
-        date: 'Sept. 11, 2018',
-        name: 'Sep112018'
-    },
-    {
-        image: Sep10Run,
-        date: 'Sept. 10, 2018',
-        name: 'Sep102018'
-    },
-    {
-        image: Sep09Run2,
-        date: 'Sept. 9, 2018 (2 of 2)',
-        name: 'Sep102018Run2'
-    },
-    {
-        image: Sep09Run1,
-        date: 'Sept. 9, 2018 (1 of 2)',
-        name: 'Sep092018Run1'
-    }    
+    },    
 ];
 
